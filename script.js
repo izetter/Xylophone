9fca1e83854958108a8b0363110d84b2394723e9
Isay Zetter <59151193+izetter@users.noreply.github.com>
2021-03-25 16:14:49 -0600

    global hasFired

---
+++ script.js
@@ -0,0 +1,107 @@
+
+// Variables
+const buttons = document.querySelectorAll('#set > button');
+const recBtn = document.querySelector('#rec');
+const playBtn = document.querySelector('#play');
+const recording = [];
+let isRecording = false;
+let hasFired = false;
+
+
+// Event listeners
+document.addEventListener('keydown', (evt) => {
+	if (!hasFired) {
+		if (/[wasdjklWASDJKL]/.test(evt.key)){
+			handler(evt.key.toLowerCase());
+			hasFired = true;
+		}
+	}
+})
+
+document.addEventListener('keyup', () => hasFired = false);
+
+
+for (let btn of buttons) {
+	btn.addEventListener('click', (evt) => handler(evt.currentTarget.id))
+}
+ 
+recBtn.addEventListener('click', record);
+
+playBtn.addEventListener('click', playRecording);
+
+
+// Functions
+
+function record() {
+	(isRecording) ? isRecording = false : isRecording = true;
+	recBtn.classList.toggle('pressed');
+	playBtn.disabled = false;
+}
+
+function playRecording() {
+	isRecording = false;
+	recBtn.classList.remove('pressed');
+	const tempRecording = [...recording];
+	const playbackInterval = setInterval(() => {
+		handler(tempRecording.shift());
+		if (tempRecording.length === 0) clearInterval(playbackInterval);
+	}, 250)
+}
+
+function handler(key) {
+
+	// Button animation
+	let btn = document.querySelector(`#${key}`);
+	btn.classList.add('pressed');
+	setTimeout(() => btn.classList.remove('pressed'), 100);
+
+	// Audio playback
+	let audio;
+	switch (key) {
+
+		case 'w': {
+			audio = new Audio('./sounds/crash.mp3');
+			break;
+		}
+
+		case 'a': {
+			audio = new Audio('./sounds/kick-bass.mp3');
+			break;
+		}
+
+		case 's': {
+			audio = new Audio('./sounds/snare.mp3');
+			break;
+		}
+
+		case 'd': {
+			audio = new Audio('./sounds/tom-1.mp3');
+			break;
+		}
+
+		case 'j': {
+			audio = new Audio('./sounds/tom-2.mp3');
+			break;
+		}
+
+		case 'k': {
+			audio = new Audio('./sounds/tom-3.mp3');
+			break;
+		}
+
+		case 'l': {
+			audio = new Audio('./sounds/tom-4.mp3');
+			break;
+		}
+	} 
+	audio.play();
+
+	// Saving keys into the saved recording
+	if (isRecording) {
+		recording.push(key);
+	}
+}
+
+
+// REFACTOR IN SUCH A WAY THAT YOU USE OBJECTS AND THERE ARE NO GLOBAL VARIABLES AND EVENT LISTENERS JUST CALL THE APPROPIEATE METHOD INSTEAD OF
+// PERFORMING OPERATIONS THEMSELVES OTHER THAN SENDING THE (MAYBE ALREADY VALIDATED) INFORMATION NEEDED
