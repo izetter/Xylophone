7f6d5921024ed438c672810511cd5f8ad579d4e7
Isay Zetter <59151193+izetter@users.noreply.github.com>
2021-06-08 21:39:05 -0500

    test

--- script.js
+++ script.js
@@ -495,5 +495,9 @@ const mobileOverlay = document.querySelector('div#portrait');
 screen.orientation.addEventListener('change', () => {
 	if (window.matchMedia("(orientation: landscape)").matches && /Android|webOS|iPhone|iPod|Opera Mini/i.test(navigator.userAgent)) {
 		mobileOverlay.style.display = 'initial';
+		alert('true');
+		alert(navigator.userAgent);
+	} else {
+		alert(navigator.userAgent);
 	}
 })
